a38cb13c3275e432be908c48a5863b11f575ce32
hoanghaianh150699 <1721050883@student.humg.edu.vn>
2021-08-24 14:56:31 +0700

    bai2

---
+++ bai2/bai2/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai2.Models;
+
+namespace bai2.Controllers
+{
+    public class DemoController : Controller
+    {
+        giaiphuongtrinh gpt = new giaiphuongtrinh();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult gptb1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult gptb1(double hesoA, double hesoB)
+        {
+            double x = gpt.giaiphuongtrinhbacnhat(hesoA, hesoB);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+    }
+}
---
+++ bai2/bai2/Models/giaiphuongtrinh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai2.Models
+{
+    public class giaiphuongtrinh
+    {
+        public double giaiphuongtrinhbacnhat(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x; 
+    }
+}
