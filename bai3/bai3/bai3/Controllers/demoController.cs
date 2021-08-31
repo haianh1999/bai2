220a46af4fbd4110120739fede484e47bbc900f1
hoanghaianh150699 <1721050883@student.humg.edu.vn>
2021-08-31 13:14:26 +0700

    cmt

---
+++ bai3/bai3/bai3/Controllers/demoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai3.Models;
+
+namespace bai3.Controllers
+{
+    public class demoController : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TinhTong(string soA, string soB)
+        {
+            int SoThuNha = Convert.ToInt32(soA);
+            int SoThuHai = Convert.ToInt32(soB);
+            int TongHaiSo = gpt.TinhTong(SoThuNha, SoThuHai);
+            ViewBag.Tong = TongHaiSo;
+            return View();
+
+        }
+    }
+}
---
+++ bai3/bai3/bai3/Models/GiaiPhuongTrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai3.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public int TinhTong(int a, int b)
+        {
+            int tong = 0;
+            tong = a + b;
+            return tong;
+        }
+    }
+}
