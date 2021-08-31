23bada69ef962467e72dae58cc8989a4e1d89a7c
hoanghaianh150699 <1721050883@student.humg.edu.vn>
2021-08-31 13:55:30 +0700

    cmt

---
+++ bai4/bai4/bai4/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai4.Models;
+
+namespace bai4.Controllers
+{
+    public class DemoController : Controller
+    {
+        GiaiPhuongTrinhBacNhat gpt = new GiaiPhuongTrinhBacNhat();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPhuongTrinhBacNhat(string soA, string soB)
+        {
+            double a = double.Parse(soA);
+            double b =double.Parse(soB);
+            double x = gpt.GiaiPhuongTrinhBacNhat(a, b);
+            ViewBag.NghiemPT = x;
+            return View();
+        }
+    }
+}
---
+++ bai4/bai4/bai4/Models/GiaiPhuongTrinhBacNhat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai4.Models
+{
+    public class GiaiPhuongTrinhBacNhat
+    {
+        public double GiaiPhuongTrinhBacNhat(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
