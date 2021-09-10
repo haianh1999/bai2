90bc6b1b5d7a6f3334817e55047b32bbf01d5ea8
hoanghaianh150699 <1721050883@student.humg.edu.vn>
2021-09-10 13:57:12 +0700

    cmt

---
+++ bai4/bai4/bai4/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai4.Models;
+
+namespace bai4.Controllers
+{
+    public class StudentController : Controller
+    {
+        LTQLDbContext db = new LTQLDbContext();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var model = db.KhachHangs.ToList();
+            return View(model);
+        }
+    }
+}
